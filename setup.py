74147c0576673cde78d27d763aa4a88ea7333ba4
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-09-04 16:05:51 +0100

    Mark the package properly and make it install the script.

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
     # Scripts
     entry_points={
         "gui_scripts": [
+            "spynnaker_raytrace = spynnaker_visualisers.raytrace.drawer:main",
             "spynnaker_sudoku = "
             "spynnaker_visualisers.sudoku.sudoku_visualiser:main",
         ],
