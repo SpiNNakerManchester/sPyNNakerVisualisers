77e96b3931467e34bf1d367453da736a1168b655
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-03-14 11:28:26 +0000

    Convert to support Python 3.
    Also enable aggressive pylint checking. BEWARE!

--- spynnaker_visualisers/glut_framework.py
+++ spynnaker_visualisers/glut_framework.py
@@ -1,34 +1,36 @@
-'''
-The MIT License
+"""
+A basic framework that implements the basic system handling for a GUI
+application that uses OpenGL and GLUT to do the GUI work.
+"""
+# The MIT License
+#
+# Copyright (c) 2010 Paul Solt, PaulSolt@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# @author: Converted to Python by Donal Fellows
 
-Copyright (c) 2010 Paul Solt, PaulSolt@gmail.com
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-@author: Converted to Python by Donal Fellows
-'''
-
-import OpenGL.GLUT as GLUT  # pylint: disable=import-error
 from datetime import datetime
-from spinn_utilities.abstract_base import AbstractBase, abstractmethod
-from six import add_metaclass
 import traceback
+from six import add_metaclass
+import OpenGL.GLUT as GLUT
+from spinn_utilities.abstract_base import AbstractBase, abstractmethod
 from spynnaker_visualisers.opengl_support import viewport, save_matrix, \
     enable, blend, line_smooth, disable, line_width, blend_function, \
     src_alpha, one_minus_src_alpha, rotate, scale, translate, raster_position
@@ -48,58 +50,73 @@ class _PerformanceTimer(object):
         self._stamp_2 = 0
 
     def start(self):
+        """ Start the timer. """
         self._stopped = False
         self._stamp_1 = _PerformanceTimer._now()
 
     def stop(self):
+        """ Stop the timer. """
         self._stamp_2 = _PerformanceTimer._now()
         self._stopped = True
 
     @property
     def stopped(self):
+        """ Is the timer stopped? """
         return self._stopped
 
     @property
-    def elapsedMilliseconds(self):
+    def elapsed_milliseconds(self):
+        """ How long elapsed in the last timing run? In milliseconds.
+
+        ..note::
+            Only valid when the timer has previously been run and is currently\
+            stopped.
+        """
         delta = self._stamp_2 - self._stamp_1
         return float(delta.seconds) * 1000 + float(delta.microseconds) / 1000
 
     @property
-    def elapsedSeconds(self):
+    def elapsed_seconds(self):
+        """ How long elapsed in the last timing run? In seconds.
+
+        ..note::
+            Only valid when the timer has previously been run and is currently\
+            stopped.
+        """
         delta = self._stamp_2 - self._stamp_1
         return float(delta.seconds) + float(delta.microseconds) / 1000000
 
 
 @add_metaclass(AbstractBase)
 class GlutFramework(object):
-    '''Base for code that wants to visualise using an OpenGL surface.
+    ''' Base for code that wants to visualise using an OpenGL surface.
     '''
+    # pylint: disable=broad-except
     __slots__ = [
-        "displayTimer",
-        "elapsedTimeInSeconds",
-        "frameRateTimer",
-        "frameTime",
-        "frameTimeElapsed",
-        "__loggederrors",
+        "display_timer",
+        "elapsed_time_in_seconds",
+        "frame_rate_timer",
+        "frame_time",
+        "frame_time_elapsed",
+        "__logged_errors",
         "window"]
 
     def __init__(self):
         self.window = None
-        self.frameTimeElapsed = 0.0
-        self.frameTime = 0.0
-        self.frameRateTimer = _PerformanceTimer()
-        self.displayTimer = _PerformanceTimer()
-        self.elapsedTimeInSeconds = 0.0
-        self.__loggederrors = set()
+        self.frame_time_elapsed = 0.0
+        self.frame_time = 0.0
+        self.frame_rate_timer = _PerformanceTimer()
+        self.display_timer = _PerformanceTimer()
+        self.elapsed_time_in_seconds = 0.0
+        self.__logged_errors = set()
 
-    def startFramework(self, args, title, width, height, posx, posy, fps):
-        """startFramework will initialize framework and start the Glut run\
-        loop. It must be called after the GlutFramework class is created to\
-        start the application."""
-        # pylint: disable=too-many-arguments
-
+    def start_framework(self, args, title, width, height, posx, posy, fps):
+        """ start_framework will initialize framework and start the GLUT run\
+            loop. It must be called after the GlutFramework class is created\
+            to start the application.
+        """
         # Sets the instance to this, used in the callback wrapper functions
-        self.frameTime = 1.0 / fps * 1000.0
+        self.frame_time = 1.0 / fps * 1000.0
 
         # Initialize GLUT
         GLUT.glutInit(args)
@@ -111,148 +128,148 @@ class GlutFramework(object):
         self.init()  # Initialize
 
         # Function callbacks with wrapper functions
-        GLUT.glutDisplayFunc(self.__displayFramework)
-        GLUT.glutReshapeFunc(self.__reshapeFramework)
+        GLUT.glutDisplayFunc(self.__display_framework)
+        GLUT.glutReshapeFunc(self.__reshape_framework)
         GLUT.glutIdleFunc(self.__run)
-        GLUT.glutMouseFunc(self.__mouseButtonPress)
-        GLUT.glutMotionFunc(self.__mouseMove)
-        GLUT.glutKeyboardFunc(self.__keyboardDown)
-        GLUT.glutKeyboardUpFunc(self.__keyboardUp)
-        GLUT.glutSpecialFunc(self.__specialKeyboardDown)
-        GLUT.glutSpecialUpFunc(self.__specialKeyboardUp)
+        GLUT.glutMouseFunc(self.__mouse_button_press)
+        GLUT.glutMotionFunc(self.__mouse_move)
+        GLUT.glutKeyboardFunc(self.__keyboard_down)
+        GLUT.glutKeyboardUpFunc(self.__keyboard_up)
+        GLUT.glutSpecialFunc(self.__special_keyboard_down)
+        GLUT.glutSpecialUpFunc(self.__special_keyboard_up)
 
         GLUT.glutMainLoop()
 
     def init(self):
-        """Initialises GLUT and registers any extra callback functions."""
+        """ Initialises GLUT and registers any extra callback functions.
+        """
         pass
 
     @abstractmethod
     def display(self, dTime):
-        """The display function is called at a specified frames-per-second\
-        (FPS). Any animation drawing code can be run in the display method.
+        """ The display function is called at a specified frames-per-second\
+            (FPS). Any animation drawing code can be run in the display method.
 
         :param dTime: the change in time (seconds)
         """
 
     def reshape(self, width, height):
-        """Called when the window dimensions change.
+        """ Called when the window dimensions change.
 
         :param width: the width of the window in pixels
         :param height: the height of the window in pixels
         """
         viewport(0, 0, width, height)
 
-    def mouseButtonPress(self, button, state, x, y):
-        """Called when the mouse buttons are pressed.
+    def mouse_button_press(self, button, state, x, y):
+        """ Called when the mouse buttons are pressed.
 
         :param button: the mouse buttons
         :param state: the state of the buttons
         :param x: the x coordinate
         :param y: the y coordinate
         """
-        pass
 
-    def mouseMove(self, x, y):
-        """Called when the mouse moves on the screen.
+    def mouse_move(self, x, y):
+        """ Called when the mouse moves on the screen.
 
         :param x: the x coordinate
         :param y: the y coordinate
         """
-        pass
 
-    def keyboardDown(self, key, x, y):
-        """The keyboard function is called when a standard key is pressed\
-        down.
-
-        :param key: the key press
-        :param x: the x coordinate of the mouse
-        :param y: the y coordinate of the mouse
-        """
-        pass
-
-    def keyboardUp(self, key, x, y):
-        """The keyboard function is called when a standard key is "unpressed".
+    def keyboard_down(self, key, x, y):
+        """ The keyboard function is called when a standard key is pressed\
+            down.
 
         :param key: the key press
         :param x: the x coordinate of the mouse
         :param y: the y coordinate of the mouse
         """
-        pass
 
-    def specialKeyboardDown(self, key, x, y):
-        """The keyboard function is called when a special key is pressed down\
-        (F1 keys, Home, Inser, Delete, Page Up/Down, End, arrow keys).\
-        http://www.opengl.org/resources/libraries/glut/spec3/node54.html
+    def keyboard_up(self, key, x, y):
+        """ The keyboard function is called when a standard key is "unpressed".
 
         :param key: the key press
         :param x: the x coordinate of the mouse
         :param y: the y coordinate of the mouse
         """
-        pass
 
-    def specialKeyboardUp(self, key, x, y):
-        """The keyboard function is called when a special key is "unpressed"\
-        (F1 keys, Home, Inser, Delete, Page Up/Down, End, arrow keys).
+    def special_keyboard_down(self, key, x, y):
+        """ The keyboard function is called when a special key is pressed down\
+            (F1 keys, Home, Inser, Delete, Page Up/Down, End, arrow keys).\
+            http://www.opengl.org/resources/libraries/glut/spec3/node54.html
 
         :param key: the key press
         :param x: the x coordinate of the mouse
         :param y: the y coordinate of the mouse
         """
-        pass
+
+    def special_keyboard_up(self, key, x, y):
+        """ The keyboard function is called when a special key is "unpressed"\
+            (F1 keys, Home, Inser, Delete, Page Up/Down, End, arrow keys).
+
+        :param key: the key press
+        :param x: the x coordinate of the mouse
+        :param y: the y coordinate of the mouse
+        """
 
     def run(self):
-        """The run method is called by GLUT and contains the logic to set the\
-        frame rate of the application.
+        """ The run method is called by GLUT and contains the logic to set the\
+            frame rate of the application.
         """
-        if self.frameRateTimer.stopped:
-            self.frameRateTimer.start()
+        if self.frame_rate_timer.stopped:
+            self.frame_rate_timer.start()
 
         # stop the timer and calculate time since last frame
-        self.frameRateTimer.stop()
-        milliseconds = self.frameRateTimer.elapsedMilliseconds
-        self.frameTimeElapsed += milliseconds
+        self.frame_rate_timer.stop()
+        milliseconds = self.frame_rate_timer.elapsed_milliseconds
+        self.frame_time_elapsed += milliseconds
 
-        if self.frameTimeElapsed >= self.frameTime:
+        if self.frame_time_elapsed >= self.frame_time:
             # If the time exceeds a certain "frame rate" then show the next
             # frame
             GLUT.glutPostRedisplay()
 
             # remove a "frame" and start counting up again
-            self.frameTimeElapsed -= self.frameTime
-        self.frameRateTimer.start()
+            self.frame_time_elapsed -= self.frame_time
+        self.frame_rate_timer.start()
 
-    def displayFramework(self):
-        """The displayFramework() function sets up initial GLUT state and\
-        calculates the change in time between each frame. It calls the\
-        display(float) function which can be subclassed.
+    def display_framework(self):
+        """ The display_framework() function sets up initial GLUT state and\
+            calculates the change in time between each frame. It calls the\
+            display(float) function, which can be subclassed.
         """
-        if self.displayTimer.stopped:
-            self.displayTimer.start()
-        self.displayTimer.stop()
-        elapsedTimeInSeconds = self.displayTimer.elapsedSeconds
+        if self.display_timer.stopped:
+            self.display_timer.start()
+        self.display_timer.stop()
+        elapsedTimeInSeconds = self.display_timer.elapsed_seconds
         if GLUT.glutGetWindow() == self.window:
             self.display(elapsedTimeInSeconds)
             GLUT.glutSwapBuffers()
-        self.displayTimer.start()
+        self.display_timer.start()
 
-    def reshapeFramework(self, width, height):
+    def reshape_framework(self, width, height):
+        """ Handle resizing of the window.
+        """
         if GLUT.glutGetWindow() == self.window:
             self.reshape(width, height)
 
-    def write_large(self, x, y, string, *args):
-        """Utility function: write a string to a given location as a bitmap.
+    @staticmethod
+    def write_large(x, y, string, *args):
+        """ Utility function: write a string to a given location as a bitmap.
         """
+        # pylint: disable=no-member
         if args:
             string = string % args
         raster_position(x, y)
         for ch in string:
             GLUT.glutBitmapCharacter(GLUT.GLUT_BITMAP_TIMES_ROMAN_24, ord(ch))
 
-    def write_small(self, x, y, size, rotation, string, *args):
-        """Utility function: write a string to a given location as a strokes.
+    @staticmethod
+    def write_small(x, y, size, rotation, string, *args):
+        """ Utility function: write a string to a given location as a strokes.
         """
-        # pylint: disable=too-many-arguments
+        # pylint: disable=no-member
         if args:
             string = string % args
 
@@ -269,62 +286,62 @@ class GlutFramework(object):
                 GLUT.glutStrokeCharacter(GLUT.GLUT_STROKE_ROMAN, ord(ch))
             disable(blend, line_smooth)
 
-    def __displayFramework(self):
+    def __display_framework(self):
         try:
-            return self.displayFramework()
+            return self.display_framework()
         except Exception:
-            self.__logerror()
+            self.__log_error()
 
-    def __reshapeFramework(self, width, height):
+    def __reshape_framework(self, width, height):
         try:
-            return self.reshapeFramework(width, height)
+            return self.reshape_framework(width, height)
         except Exception:
-            self.__logerror()
+            self.__log_error()
 
     def __run(self):
         try:
             return self.run()
         except Exception:
-            self.__logerror()
+            self.__log_error()
 
-    def __mouseButtonPress(self, button, state, x, y):
+    def __mouse_button_press(self, button, state, x, y):
         try:
-            return self.mouseButtonPress(button, state, x, y)
+            return self.mouse_button_press(button, state, x, y)
         except Exception:
-            self.__logerror()
+            self.__log_error()
 
-    def __mouseMove(self, x, y):
+    def __mouse_move(self, x, y):
         try:
-            return self.mouseMove(x, y)
+            return self.mouse_move(x, y)
         except Exception:
-            self.__logerror()
+            self.__log_error()
 
-    def __keyboardDown(self, key, x, y):
+    def __keyboard_down(self, key, x, y):
         try:
-            return self.keyboardDown(key, x, y)
+            return self.keyboard_down(key, x, y)
         except Exception:
-            self.__logerror()
+            self.__log_error()
 
-    def __keyboardUp(self, key, x, y):
+    def __keyboard_up(self, key, x, y):
         try:
-            return self.keyboardUp(key, x, y)
+            return self.keyboard_up(key, x, y)
         except Exception:
-            self.__logerror()
+            self.__log_error()
 
-    def __specialKeyboardDown(self, key, x, y):
+    def __special_keyboard_down(self, key, x, y):
         try:
-            return self.specialKeyboardDown(key, x, y)
+            return self.special_keyboard_down(key, x, y)
         except Exception:
-            self.__logerror()
+            self.__log_error()
 
-    def __specialKeyboardUp(self, key, x, y):
+    def __special_keyboard_up(self, key, x, y):
         try:
-            return self.specialKeyboardUp(key, x, y)
+            return self.special_keyboard_up(key, x, y)
         except Exception:
-            self.__logerror()
+            self.__log_error()
 
-    def __logerror(self):
+    def __log_error(self):
         tb = traceback.format_exc()
-        if tb not in self.__loggederrors:
-            self.__loggederrors.add(tb)
+        if tb not in self.__logged_errors:
+            self.__logged_errors.add(tb)
             traceback.print_exc()
