bf5de9a3b52eb4423d48ef62775368a49ad9c222
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-02-13 09:56:19 +0000

    apply apache 2 license

--- spynnaker_visualisers/glut_framework.py
+++ spynnaker_visualisers/glut_framework.py
@@ -2,27 +2,22 @@
 A basic framework that implements the basic system handling for a GUI
 application that uses OpenGL and GLUT to do the GUI work.
 """
-# The MIT License
 #
-# Copyright (c) 2010 Paul Solt, PaulSolt@gmail.com
+# Copyright (c) 2010-2023 The University of Manchester
+# Based on work Copyright (c) 2010 Paul Solt, PaulSolt@gmail.com
+# Originally released under the MIT License
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # @author: Converted to Python by Donal Fellows
 
